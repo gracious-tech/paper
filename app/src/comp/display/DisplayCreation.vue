71dd8dc853fc0a56f3163d9061912334ede08b1e
shadow-light <noreply@gracious.tech>
2024-10-22 21:40:57 +1100

    Wording

--- app/src/comp/display/DisplayCreation.vue
+++ app/src/comp/display/DisplayCreation.vue
@@ -5,7 +5,7 @@ iframe(v-if='iframe_src' :src='iframe_src')
 div.explain(v-else :class='{pending: status === "pending"}')
     template(v-if='status === undefined')
     template(v-else-if='status === "pending"')
-        h3(class='text-h4') Generating your creation...
+        h3(class='text-h4') Preparing some good news...
         AnimatedBook
         h1(class='my-10 text-h1') {{ time_since_request }}
         div(class='mb-10')
