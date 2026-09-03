141da2ea0cc82b40dd9ba18ef689947ea3ac4110
shadow-light <noreply@gracious.tech>
2026-09-03 22:13:16 +1000

    font display fix

--- app/src/comp/global/AppFontSelect.vue
+++ app/src/comp/global/AppFontSelect.vue
@@ -5,7 +5,7 @@ v-select(v-model='model' :items='items' :label='label' :hint='hint')
     template(#item='{internalItem: item, props: item_props}')
         v-list-item(v-bind='item_props')
             template(#title v-if='typeof item.raw === "string"')
-                div(class='font-sample' :style='{fontFamily: item.raw}') {{ example_text }}
+                div(class='font-sample' :style='{fontFamily: `"${item.raw}"`}') {{ example_text }}
                 div(class='font-name text-body-small text-medium-emphasis') {{ item.raw }}
     template(#append-item)
         v-divider
